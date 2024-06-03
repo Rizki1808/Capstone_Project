604b87bcf048b9c56b6d9cd98b0eecbaefbd9f95
Rizki1808 <124553947+Rizki1808@users.noreply.github.com>
2024-06-03 20:14:16 +0700

    Menghapus 1 Comment

--- app/src/main/java/com/example/capstoneproject/MainActivity.kt
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
