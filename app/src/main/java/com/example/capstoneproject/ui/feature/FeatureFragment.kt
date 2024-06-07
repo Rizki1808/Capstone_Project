4fd7cc8d223bfca8bef75f5ff4b3e9d22d99948b
Rizki1808 <124553947+Rizki1808@users.noreply.github.com>
2024-06-07 21:22:57 +0700

    Memperbaharui Header

--- app/src/main/java/com/example/capstoneproject/ui/feature/FeatureFragment.kt
+++ app/src/main/java/com/example/capstoneproject/ui/feature/FeatureFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.capstoneproject.databinding.FragmentDashboardBinding
+import com.example.capstoneproject.databinding.FragmentFeatureBinding
 
 class FeatureFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFeatureBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FeatureFragment : Fragment() {
         val featureViewModel =
             ViewModelProvider(this).get(FeatureViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFeatureBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
